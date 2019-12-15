a9065470f0dca09839256957b266405ea36e880f
jkhartshorne <6118115+jkhartshorne@users.noreply.github.com>
2019-12-15 15:18:14 -0500

    bugfix

--- src/controllerBuilder.js
+++ src/controllerBuilder.js
@@ -20,8 +20,7 @@ export default class ControllerBuilder {
 	}
 
 	// allow users to set their own custom api endpoints that just pass things along
-	setCustomPass(route, handler, queue, httpMethod) {
-		httpMethod = httpMethod || 'post';
+	setCustomPass(route, rpcMethod, queue, httpMethod) {
 		if (this.validHttpMethods.indexOf(httpMethod) < 0) {
 			console.error(`'${httpMethod}' is not a valid http method. Ignoring for route ${route}`);
 			return;
